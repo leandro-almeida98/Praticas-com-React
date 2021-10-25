e1c4365ce3361c0f5e9c463f0fa049e408c75387
Leandro Almeida <39975179+leandro-almeida98@users.noreply.github.com>
2021-10-25 09:40:41 -0300

    Update App.js
    Flexbox e Alinhamentos

--- App.js
+++ App.js
@@ -8,11 +8,35 @@ class App extends Component{
 
     let img = 'https://sujeitoprogramador.com/steve.png';
     return(
-       <View style={{flex:1, backgroundColor:'#ddd'}}>
+       <View style={{flex:1, backgroundColor:'#ddd', paddingTop:31}}>
 
-<View style={{height:65 , backgroundColor: 'blue'}}></View>
-         <View style={{flex:1, backgroundColor: 'red'}}></View>
-         <View style={{height:60 , backgroundColor: 'green'}}></View>
+        <View style={{height:60 , flexDirection:'row', backgroundColor: 'blue', padding:5}}>
+          <View style={{flex:1, backgroundColor: 'red',padding:2, justifyContent: 'center', alignItems: 'flex-start'}}>
+            <View style={{width:40, height:40, backgroundColor: 'blue', }}>
+            
+            </View>
+          </View>
+          <View style={{flex:7, backgroundColor: 'green', padding:2, justifyContent: 'center', alignItems: 'center'}}>
+            <View style={{width:250, height:40, backgroundColor: 'gray', }}>
+            
+            </View>
+          </View>
+          
+          <View style={{flex:1, backgroundColor: 'gray', padding:2, justifyContent: 'center', alignItems: 'flex-end'}}>
+            <View style={{width:40, height:40, backgroundColor: 'red', }}>
+            
+            </View>
+          </View>
+          
+          
+
+        </View>
+        <View style={{flex:1, backgroundColor: 'red'}}></View>
+        <View style={{height:60 , backgroundColor: 'green', flexDirection: 'row'}}>
+          <View style={{flex:1, borderColor: 'red', borderWidth:1}}></View>
+          <View style={{flex:1, borderColor: 'red', borderWidth:1}}></View>
+          <View style={{flex:1, borderColor: 'red', borderWidth:1}}></View>
+        </View>
          
 
 
