4e979c667c6935add093196231bf343be73a394a
Leandro Almeida <39975179+leandro-almeida98@users.noreply.github.com>
2021-11-14 12:04:46 -0300

    API ENVIA FILE
    Criado com a biblioteca Axios, uma API de enviar Files via "multipart/form-data" para a Webservice PHP criada previamente.

--- App.js
+++ App.js
@@ -1,46 +1,119 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, Switch } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import React, {useState, useEffect, useRef} from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, SafeAreaView} from 'react-native';
+import { Camera } from 'expo-camera';
+import {FontAwesome} from '@expo/vector-icons';
+import { Header } from 'react-native/Libraries/NewAppScreen';
+import axios from 'axios';
 
 export default function App() {
 
-  const Stack = createNativeStackNavigator();
-  const [status, setStatus] = useState();
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [hasPermission, setHasPermission] = useState(null)
+  const camRef = useRef(null)
+  const [capturedPhoto, setCapturedPhoto] = useState(null)
+  const [nome, setNome] = useState('')
+  const serverURL = 'https://webservice.inbitdev.com/?uploadfile'
 
-  
+  useEffect(() => {
+
+
+    (async () =>{
+      const {status} = await Camera.requestCameraPermissionsAsync()
+      setHasPermission(status === 'granted')
+    })()
+
+  }, [])
+
+  if(hasPermission === null){
+    return <View/>
+  }
+
+  if(hasPermission === false){
+    return <Text>Acesso negado!</Text>
+  }
+
+  async function takePicture(){
+
+    if(camRef){
+        const data = await camRef.current.takePictureAsync()
+        setCapturedPhoto(data.uri)
+        let array = data.uri.split('/')
+        setNome(array[array.length - 1])
+        // console.log(data)
+
+        let formdata = new FormData();
+
+        formdata.append('image', {
+          uri: data.uri,
+         type: 'image/png', 
+         name: array[array.length - 1].split('.')[0],
+         tmp_name: data.uri
+         
+       });
+
+       axios({
+        url    : 'http://192.168.3.118/webservice/?uploadfile',
+        method : 'POST',
+        data   : formdata,
+        headers: {
+                     Accept: 'application/json',
+                     'Content-Type': 'multipart/form-data',
+                     'Authorization':'Basic YnJva2VyOmJyb2tlcl8xMjM='
+                 }
+             })
+             .then(function (response) {
+                     console.log("response :", response.data);
+            })
+            .catch(function (error) {
+                     console.log("error from image :");
+            })
+    }
+
+  }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
 
-      <Text>SWITCH ABAIXO</Text>
-      <Switch
-      // style={{ba}}
-        trackColor={{true: 'red', false: 'grey'}}
-        thumbColor={{true: 'blue', false: 'green'}}
-        value={status}
-        onValueChange={ (valorSwitch) => setStatus(valorSwitch)}
+        <Camera 
 
-      />
-      <Text>{(status)? "Ativo": "Inativo"}</Text>
+            style={{flex: 1}}
+            type={type}
+            ref={camRef}
+        >
+            <View style={{flex:1,backgroundColor: 'transparent', flexDirection: 'row'}}>
+                  <TouchableOpacity onPress={() => {
+                    setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back)
+                    }
+                    } style={{position: 'absolute',bottom: 20, left: 20}}>
+                          <Text style={{fontSize: 20, marginBottom: 13, color: '#FFF'}}> Trocar </Text>
+                  </TouchableOpacity>
+            </View>
+        </Camera>
 
-      
+        <TouchableOpacity style={styles.button} onPress={takePicture} >
+                <FontAwesome name="camera" size={23} color="#FFF" />
+        </TouchableOpacity>
 
-    </View>
+        <Image style={{width: 250, height: 250, borderRadius: 125}} source={{uri: capturedPhoto}}/>
+
+        <Text>{nome}</Text>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  header:{
-    marginTop:30,
-    height:60, 
-    backgroundColor: 'green',
-    justifyContent:'center', 
-    alignItems:'center'
+  container: {
+    flex: 1,
+    justifyContent: 'center'
   },
-  container:{
-    flex:1,
-    marginTop:30
+  button:{
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#121212',
+    margin: 20,
+    borderRadius: 10,
+    height: 50
   }
 });
-
